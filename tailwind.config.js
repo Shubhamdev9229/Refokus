a62ad089219170ab43bffa05ca7927c84743e10b
Shubham Singh <160633829+Shubhamdev9229@users.noreply.github.com>
2024-04-22 00:53:11 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
-/** @type {import('tailwindcss').Config} */
+s/** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./Components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
     fontFamily: {
